58185c8e39e10d193dcb18a9bb1bedf2f7b33f6d
Kristoffer A. Wright <kris.al.wright@gmail.com>
2022-05-07 23:31:40 -0700

    Add changelog

--- src/xerrh.h
+++ src/xerrh.h
@@ -13,60 +13,104 @@ the exit code that is used when the code is used with the `xe_fatal` function.
 */
 typedef enum {
 
-    // Non-error:
+    // No Error
     xe_OK = 0,
 
-    // General-errors:
+    // General Error
     xe_GEN = 1,
+
+    // Runtime Error
     xe_RUNTM = 1,
     
-    // Memory:
+    // Memory Error
     xe_MEM = 3,
 
-    // Math:
+    // Math Error
     xe_MATH = 4,
+
+    // Overflow Error
     xe_OVER = 5,
+
+    // Underflow Error
     xe_UNDER = 6,
+
+    // Floating Point Error
     xe_FLOAT = 7,
+
+    // Divide-by-Zero Error
     xe_DIVZ = 8,
 
-    // Buffer:
+    // Buffer Error
     xe_BUF = 9,
+
+    // End of File Error
     xe_EOF = 10,
+
+    // End of Stream Error
     xe_EOS = 11,
 
-    // Lookup:
+    // Lookup Error
     xe_LOOK = 12,
+
+    // Index Error
     xe_INDEX = 13,
+
+    // Key Error
     xe_KEY = 14,
 
-    // IO:
+    // IO Error
     xe_IO = 15,
+
+    // File Not Found Error
     xe_NOFILE = 16,
+
+    // Directory Not Found Error
     xe_NODIR = 17,
 
-    // Input:
+    // Input Error
     xe_IN = 18,
+
+    // Bad Data Error
     xe_DATA = 19,
+
+    // Bad Type Error
     xe_TYPE = 20,
 
-    // Service:
+    // External Service Error
     xe_EXT = 21,
+
+    // Connection Error
     xe_CNXN = 22,
+
+    // Authentication Error
     xe_AUTH = 23,
+
+    // Lost Resource Error
     xe_LOST = 24,
+
+    // Conflict Error
     xe_CONFL = 25,
 
-    // Other:
+    // OS Error
     xe_OS = 26,
+
+    // Network Error
     xe_NET = 27,
+
+    // Not Implemented Error
     xe_NIMPL = 28,
+
+    // Obsolete Error
     xe_OBS = 29,
 
-    // Testing:
+    // Special code used by test harness to find skipped tests.
     xe_TESTSKIP = 100,
+
+    // Special code used by test harness to find todo tests.
     xe_TESTTODO = 101,
-    xe_FORCE = 102
+
+    // Special code used by test harness to find test force fails.
+    xe_TESTFORCE = 102
 
 } xe_Code;
 
